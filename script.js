855bce0a32cf87ffd38c41b6f24de8f9643dd9b7
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-07-06 11:24:25 -0400

    Prompt password related questions after 'Generate Passord' btn clicked

--- script.js
+++ script.js
@@ -1,193 +1,186 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Query user for password criteria and confirm
-const lc = confirm("Do you want to include lower case characters in password");
-const uc = confirm("Do you want to include upper case characaters in password?");
-const num = confirm("Do you want to include numerals in password?");
-const sp = confirm("Do you want to include special characters in password?");
+function generatePassword() {
+  // Query user for password criteria and confirm
+  const lc = confirm(
+    "Do you want to include lower case characters in password"
+  );
+  const uc = confirm(
+    "Do you want to include upper case characaters in password?"
+  );
+  const num = confirm("Do you want to include numerals in password?");
+  const sp = confirm("Do you want to include special characters in password?");
 
-let userContinue = false;
-let pswLen = 0;
-let pswLenRaww =0;
-let wantToContinue = true;
-let inputErroror = false;
+  let userContinue = false;
+  let pswLen = 0;
+  let pswLenRaww = 0;
+  let wantToContinue = true;
+  let inputErroror = false;
 
-if (lc || uc || num || sp) {
-  userContinue=true;
-}
-// prompt user and get desired password length
-if (userContinue) {
-  
-  pswLen = prompt("Provide password length 8 - 128 chars");
-  // console.log ('PSWLEN-1:', pswLen);
+  if (lc || uc || num || sp) {
+    userContinue = true;
+  }
+  // prompt user and get desired password length
+  if (userContinue) {
+    pswLen = prompt("Provide password length 8 - 128 chars");
+    // console.log ('PSWLEN-1:', pswLen);
 
-  if (pswLen !== null) {
-  // console.log ('PSWLEN-2:', pswLen);
-    if (isNaN(pswLen) ===  true) {
+    if (pswLen !== null) {
       // console.log ('PSWLEN-2:', pswLen);
-      inputError = true;
-      alert("Please enter a number"); 
-    }
-    else {
-      inputError =  (pswLen < 8 || pswLen > 128);
-    }
-    /*
+      if (isNaN(pswLen) === true) {
+        // console.log ('PSWLEN-2:', pswLen);
+        inputError = true;
+        alert("Please enter a number");
+      } else {
+        inputError = pswLen < 8 || pswLen > 128;
+      }
+      /*
     console.log ('PSWLEN-2:', pswLen);
     pswLen = pasrsInt(pswLen);
     console.log ('PSWLEN-2:', pswLen);
     inputError = (pswLen < 8 || pswLen > 128);
 */
 
-    while (userContinue && inputError) {
+      while (userContinue && inputError) {
+        alert("Password Length is a number between 8 and 128.");
 
-      alert("Password Length is a number between 8 and 128.");
+        wantToContinue = confirm("Would you like to continue?");
 
-      wantToContinue = confirm("Would you like to continue?");
-
-      if (wantToContinue === false) {
-        alert( "No passord will be generated; valid psw length needed (8 - 128 chars)");
-        userContinue = false;
-        
-      }
-      else {
-        pswLen = prompt("Provide password length 8 - 128 chars");
-
-        if (pswLen !== null)
-        {
-         // console.log ('PSWLEN-2:', pswLen);
-        inputError= (((isNaN(pswLen)) || (pswLen <8 || pswLen >128)));
-        }
-        else {
+        if (wantToContinue === false) {
+          alert(
+            "No passord will be generated; valid psw length needed (8 - 128 chars)"
+          );
           userContinue = false;
+        } else {
+          pswLen = prompt("Provide password length 8 - 128 chars");
+
+          if (pswLen !== null) {
+            // console.log ('PSWLEN-2:', pswLen);
+            inputError = isNaN(pswLen) || pswLen < 8 || pswLen > 128;
+          } else {
+            userContinue = false;
+          }
         }
-        
       }
-
+    } else {
+      userContinue = false;
     }
+    if (wantToContinue && userContinue && pswLen !== null) {
+      alert(
+        `Your selected password criteria:\n    include lowercase = ${lc}\n    include upper case = ${uc}\n    include numbers = ${num}\n    include special characters = ${sp}\n    password Length = ${pswLen}\n\nClick on Generate Password Button to generate your secure passord with these password criteria`
+      );
+    }
+  } else {
+    alert(
+      "One of lower case, upper case, numerals or special characters have to be selected to generate Password, Please refresh page and re-enter"
+    );
   }
-  else {
-    userContinue = false;
+
+  let errorText = "";
+  if (!userContinue) {
+    errorText += "No password Generated";
+    if (!(lc || uc || num || sp)) {
+      errorText +=
+        ", Atleast one char type should be included for Password Generation.";
+    } else if (wantToContinue === false) {
+      errorText += ", User Cancelled.";
+    } else if (pswLen === null) {
+      errorText += ", No pswLen provided / User Cacelled";
+    }
+    // console.log('PSWLEN=',pswLen);
+    errorText += " Reload the page and retry.";
+    return errorText;
   }
-  if ((wantToContinue) && (userContinue) && (pswLen !== null)){
-  alert(`Your selected password criteria:\n    include lowercase = ${lc}\n    include upper case = ${uc}\n    include numbers = ${num}\n    include special characters = ${sp}\n    password Length = ${pswLen}\n\nClick on Generate Password Button to generate your secure passord with these password criteria`);
-}
-}
-else {
-  alert("One of lower case, upper case, numerals or special characters have to be selected to generate Password, Please refresh page and re-enter");
-}
 
+  // We are here means, all user inputs are validated and we are ready to
+  // generate the password per user password criteria
 
-function generatePassword() {
-let errorText = '';
-if (!userContinue) {
-  errorText += 'No password Generated';
-if (!(lc || uc || num || sp)) {
-  errorText += ', Atleast one char type should be included for Password Generation.';
-}
+  // different possible character sets
+  const lcChars = "abcdefghijklmnopqrstuvwxyz";
+  const ucChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numerals = "0123456789";
+  const spChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
+  // possiblePswChars builds from empty string
+  // Initialize other variables as well
+  let possiblePswChars = "";
+  let generatedPsw = "";
+  let i = 0;
 
-else if (wantToContinue === false) {
-errorText += ', User Cancelled.';
-}
-else if (pswLen === null) {
-  errorText += ', No pswLen provided / User Cacelled';
-}
-// console.log('PSWLEN=',pswLen);
-errorText += ' Reload the page and retry.';
-  return errorText;
-}
-
-// We are here means, all user inputs are validated and we are ready to
-// generate the password per user password criteria
-
-// different possible character sets
-const lcChars = "abcdefghijklmnopqrstuvwxyz";
-const ucChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numerals = '0123456789';
-const spChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-
-// possiblePswChars builds from empty string 
-// Initialize other variables as well
-let possiblePswChars = '';
-let generatedPsw = '';
-let i=0;
-
-/* if user wants to include lowercase characters, let us choose a lowercase character
+  /* if user wants to include lowercase characters, let us choose a lowercase character
  from among the 24 randomly and use it as 1st char in password
  We are trying to also honor ( self imposed restriction or a not a must requirement) that
  if user wants a character type to be included in the password, make sure atleast 1 character
  from that character type is present in the password  */
-if (lc) {
-  possiblePswChars = lcChars;
-  generatedPsw += lcChars[Math.floor(Math.random() *lcChars.length)];
-  i++;
-  // console.log('POSSIBLEPSWCHARS', possiblePswChars);
-  // console.log('GENERATED PSW', generatedPsw);
-} 
+  if (lc) {
+    possiblePswChars = lcChars;
+    generatedPsw += lcChars[Math.floor(Math.random() * lcChars.length)];
+    i++;
+    // console.log('POSSIBLEPSWCHARS', possiblePswChars);
+    // console.log('GENERATED PSW', generatedPsw);
+  }
 
-/* if user wants to include uppercase characters, let us choose a uppercase character
+  /* if user wants to include uppercase characters, let us choose a uppercase character
  from among the 24 randomly and use it as 2nd char in password
  We are trying to also honor ( self imposed restriction or a not a must requirement) that
  if user wants a character type to be included in the password, make sure atleast 1 character
  from that character type is present in the password */
-if (uc) {
-  possiblePswChars = possiblePswChars + ucChars;
-  generatedPsw += ucChars[Math.floor(Math.random() *ucChars.length)];
-  i++;
-  // console.log('POSSIBLEPSWCHARS', possiblePswChars);
-  // console.log('GENERATED PSW', generatedPsw);
-  
-}
+  if (uc) {
+    possiblePswChars = possiblePswChars + ucChars;
+    generatedPsw += ucChars[Math.floor(Math.random() * ucChars.length)];
+    i++;
+    // console.log('POSSIBLEPSWCHARS', possiblePswChars);
+    // console.log('GENERATED PSW', generatedPsw);
+  }
 
-/* if user wants to include numbers, let us choose a number
+  /* if user wants to include numbers, let us choose a number
  from among the 10 randomly and use it as 3rd char in password
  We are trying to also honor ( self imposed restriction or a not a must requirement) that
  if user wants a character type to be included in the password, make sure atleast 1 character
  from that character type is present in the password */
-if (num) {
-  possiblePswChars = possiblePswChars + numerals;
-  generatedPsw += numerals[Math.floor(Math.random() *numerals.length)];
-  i++;
-  // console.log('POSSIBLEPSWCHARS',possiblePswChars);
-  // console.log('GENERATED PSW', generatedPsw);
-}
+  if (num) {
+    possiblePswChars = possiblePswChars + numerals;
+    generatedPsw += numerals[Math.floor(Math.random() * numerals.length)];
+    i++;
+    // console.log('POSSIBLEPSWCHARS',possiblePswChars);
+    // console.log('GENERATED PSW', generatedPsw);
+  }
 
-/* if user wants to include special characters, let us choose a number
+  /* if user wants to include special characters, let us choose a number
  from among all the sepcial characters and use it as  4th char in password
  We are trying to also honor ( self imposed restriction or a not a must requirement) that
  if user wants a character type to be included in the password, make sure atleast 1 character
  from that character type is present in the password */
-if (sp) {
-  possiblePswChars = possiblePswChars + spChars;
-  generatedPsw += spChars[Math.floor(Math.random() *spChars.length)];
-  i++;
-  // console.log('POSSIBLEPSWCHARS',possiblePswChars);
+  if (sp) {
+    possiblePswChars = possiblePswChars + spChars;
+    generatedPsw += spChars[Math.floor(Math.random() * spChars.length)];
+    i++;
+    // console.log('POSSIBLEPSWCHARS',possiblePswChars);
+    // console.log('GENERATED PSW', generatedPsw);
+  }
+
+  // console.log('I:', i);
+  // console.log('PSWLEN',pswLen);
+
+  /* Now that we ensured atleast one character of each character type that the user want to include in the password is built in to the password, build the rest of the password in a random manner from all the character types that the user wanted to make up the password */
+  for (; i < pswLen; i++) {
+    generatedPsw +=
+      possiblePswChars[Math.floor(Math.random() * possiblePswChars.length)];
+  }
   // console.log('GENERATED PSW', generatedPsw);
+  return generatedPsw;
 }
-
-// console.log('I:', i);
-// console.log('PSWLEN',pswLen);
-
-/* Now that we ensured atleast one character of each character type that the user want to include in the password is built in to the password, build the rest of the password in a random manner from all the character types that the user wanted to make up the password */
-for (; i<pswLen;i++) {
-  generatedPsw += possiblePswChars[Math.floor(Math.random() *possiblePswChars.length)];
-}
-// console.log('GENERATED PSW', generatedPsw);
-return(generatedPsw);
-}
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   // console.log('PASSWORD:', password);
   var passwordText = document.querySelector("#password");
- // console.log('PASSWORDTEXT:', passwordText);
- // console.log('password:',password);
+  // console.log('PASSWORDTEXT:', passwordText);
+  // console.log('password:',password);
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
